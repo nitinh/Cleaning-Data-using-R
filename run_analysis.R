918ee3eda3bfe5c181874d9ae24f4b92ef7f4dbc
DAMODAR HEGDE <dhegde@DAMODARs-MacBook-Pro.local>
2014-04-27 15:54:54 -0700

    add R script and tidy data

--- run_analysis.R
+++ run_analysis.R
@@ -1,4 +1,4 @@
-fileurl="https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+fctivity_labelsileurl="https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
 download.file(fileurl,destfile="wearable.zip",method="curl")
 unzip("wearable.zip")
 
@@ -14,18 +14,20 @@ x_train_in <- read.table(file="UCI\ HAR\ Dataset/train/X_train.txt",col.names=as
 x_train <- x_train_in[,meanstdcols]
 subject_train <- read.table(file="UCI\ HAR\ Dataset/train/subject_train.txt",col.names=c("subject"))
 
-activity = merge(y_train,activity_labels,by.x="activityid",by.y="id")
-data_train <- cbind(x_train,subject_train,activity)
-
 y_test <- read.table(file="UCI\ HAR\ Dataset/test/Y_test.txt",col.names=c("activityid"))
 x_test_in <- read.table(file="UCI\ HAR\ Dataset/test/X_test.txt",col.names=as.vector(features$name))
 x_test <- x_test_in[,meanstdcols]
 subject_test <- read.table(file="UCI\ HAR\ Dataset/test/subject_test.txt",col.names=c("subject"))
 
-activity= merge(y_test,activity_labels,by.x="activityid",by.y="id")
-data_test <- cbind(x_test,subject_test,activity)
+subject <- rbind(subject_train,subject_test)
+y_all <- rbind(y_train,y_test)
+adt <- cbind(subject,y_all)
 
-all_data <- rbind(data_train,data_test)
+x_data <- rbind(x_train,x_test)
+
+activity = merge(adt,activity_labels,by.x="activityid",by.y="id")
+
+all_data <- cbind(x_data,activity)
 
 all_data <- all_data[, !(colnames(all_data) %in% c("activityid"))]
 
